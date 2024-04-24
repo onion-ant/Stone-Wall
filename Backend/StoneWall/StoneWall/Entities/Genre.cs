e4887eb127f72e9f9626c1ece7c7a5b9b397e039
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-04-24 17:45:25 -0300

    Change item and add genre

---
+++ Backend/StoneWall/StoneWall/Entities/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoneWall.Entities
+{
+    public class Genre
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string? Name { get; set; }
+    }
+}
