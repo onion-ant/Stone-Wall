34025ebb3578caefe23a7e9dc9d2ce822825e801
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-04-28 15:32:16 -0300

    Create GetAddonsAsync method

--- Backend/StoneWall/StoneWall/Services/IStreamingServicesService.cs
+++ Backend/StoneWall/StoneWall/Services/IStreamingServicesService.cs
@@ -7,6 +7,7 @@ namespace StoneWall.Services
     public interface IStreamingServicesService
     {
         public Task<List<StreamingServices>> GetStreamingsAsync();
+        public Task<List<Addon>> GetAddonsAsync(string streamingId);
         public Task<ItemStreamingPaginationHelper> GetItemsAsync(string streamingId, int pageNumber);
     }
 }
