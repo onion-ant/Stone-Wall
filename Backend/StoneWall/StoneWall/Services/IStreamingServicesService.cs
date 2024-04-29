038ddc9c275440865924ceccfe1e91fecf9700da
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-04-29 02:25:02 -0300

    Create CompareStreamings method

--- Backend/StoneWall/StoneWall/Services/IStreamingServicesService.cs
+++ Backend/StoneWall/StoneWall/Services/IStreamingServicesService.cs
@@ -9,5 +9,6 @@ namespace StoneWall.Services
         public Task<List<StreamingServices>> GetStreamingsAsync();
         public Task<List<Addon>> GetAddonsAsync(string streamingId);
         public Task<ItemStreamingPaginationHelper> GetItemsAsync(string streamingId, int pageNumber);
+        public Task<ItemStreamingPaginationHelper> CompareStreamings(string streamingExclusivo, string strimingExcluido, int page);
     }
 }
--- Backend/StoneWall/StoneWall/Services/StreamingServicesService.cs
+++ Backend/StoneWall/StoneWall/Services/StreamingServicesService.cs
@@ -3,6 +3,7 @@ using StoneWall.Data;
 using StoneWall.Entities;
 using StoneWall.Helpers;
 using StoneWall.Services.Exceptions;
+using System.Linq;
 
 namespace StoneWall.Services
 {
@@ -10,12 +11,12 @@ namespace StoneWall.Services
     {
         private readonly StoneWallDbContext _context;
         private readonly int _pageSize;
-        public StreamingServicesService(StoneWallDbContext context,IConfiguration config)
+        public StreamingServicesService(StoneWallDbContext context, IConfiguration config)
         {
             _context = context;
             _pageSize = int.Parse(config["PageSize"]);
         }
-        
+
         public async Task<List<StreamingServices>> GetStreamingsAsync()
         {
             var streamings = await _context.Streaming_Services.AsNoTracking().ToListAsync();
@@ -36,6 +37,11 @@ namespace StoneWall.Services
         }
         public async Task<ItemStreamingPaginationHelper> GetItemsAsync(string streamingId, int pageNumber)
         {
+            int totalPages = await GetTotalPages(streamingId);
+            if ((totalPages < pageNumber || pageNumber == 0) && totalPages != 0)
+            {
+                throw new PageException("Invalid pageNumber");
+            }
             var streamingItems = await _context.Item_Streaming.AsNoTracking().Include(Is => Is.Item).Where(Is => Is.StreamingId == streamingId)
                .Select(Is => new ItemStreaming
                {
@@ -54,16 +60,10 @@ namespace StoneWall.Services
                 .Skip((pageNumber - 1) * _pageSize)
                 .Take(_pageSize)
                 .ToListAsync();
-            int totalPages = await GetTotalPages(streamingId);
             if (!streamingItems.Any())
             {
-                throw new NotFoundException("Theres no registered item from this streaming");
+                throw new NotFoundException($"Theres no registered item from {streamingId}");
             }
-            if (totalPages < pageNumber)
-            {
-                throw new PageException("Maximum page number exceeded");
-            }
-
             var response = new ItemStreamingPaginationHelper()
             {
                 ItemsStreaming = streamingItems,
@@ -71,13 +71,59 @@ namespace StoneWall.Services
             };
             return response;
         }
-
-        private async Task<int> GetTotalPages(string streamingId)
+        public async Task<ItemStreamingPaginationHelper> CompareStreamings(string streamingExclusive, string streamingExcluded, int pageNumber)
         {
-            int totalPaginas = (await _context.Item_Streaming
+            int totalPages = await GetTotalPages(streamingExclusive, streamingExcluded);
+            if ((totalPages < pageNumber || pageNumber == 0) && totalPages != 0)
+            {
+                throw new PageException("Invalid pageNumber");
+            }
+            var ExclusiveItems = await _context.Item_Streaming
+                    .Where(Is => Is.StreamingId == streamingExclusive &&
+                        !_context.Item_Streaming.Any(Is2 => Is2.StreamingId == streamingExcluded && Is2.Item.TmdbId == Is.Item.TmdbId))
+                    .Select(Is => new ItemStreaming
+                    {
+                        Item = new Item
+                        {
+                            TmdbId = Is.Item.TmdbId,
+                            Title = Is.Item.Title,
+                            OriginalTitle = Is.Item.OriginalTitle,
+                            Popularity = Is.Item.Popularity,
+                            Type = Is.Item.Type
+                        },
+                        Type = Is.Type,
+                        Link = Is.Link,
+                    })
+                    .OrderByDescending(Is => Is.Item.Popularity)
+                    .Skip((pageNumber - 1) * _pageSize)
+                    .Take(_pageSize)
+                    .ToListAsync();
+            if (!ExclusiveItems.Any())
+            {
+                throw new NotFoundException($"Theres no items in {streamingExclusive} that dont have in the {streamingExcluded}");
+            }
+            var response = new ItemStreamingPaginationHelper()
+            {
+                ItemsStreaming = ExclusiveItems,
+                LastPage = totalPages,
+            };
+            return response;
+        }
+        private async Task<int> GetTotalPages(string streamingId, string? streamingExcluded = null)
+        {
+            int totalPages = 0;
+            if (string.IsNullOrWhiteSpace(streamingId))
+            {
+                totalPages = (await _context.Item_Streaming
                 .AsNoTracking()
-                .CountAsync(Is => Is.StreamingId == streamingId) + _pageSize - 1)/_pageSize;
-            return totalPaginas;
+                .CountAsync(Is => Is.StreamingId == streamingId) + _pageSize - 1) / _pageSize;
+                return totalPages;
+            }
+            totalPages = (await _context.Item_Streaming
+                .AsNoTracking()
+                .CountAsync(Is => Is.StreamingId == streamingId &&
+                        !_context.Item_Streaming.Any(Is2 => Is2.StreamingId == streamingExcluded && Is2.Item.TmdbId == Is.Item.TmdbId)) + _pageSize - 1) / _pageSize;
+            return totalPages;
         }
     }
 }
