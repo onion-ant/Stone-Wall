34d4ba3a04e5943d3fef6775785a409e61b915e9
Felipe Ferreira <fefelipe21@gmail.com>
2024-04-30 19:45:51 -0300

    add moviesHomePage

---
+++ StoneWall/src/Component/Movies/Movies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Movies.module.css';
+
+const Movies = () => {
+  return (
+    <div className={styles.background}>
+      <div className={`${styles.movies} 'container'`}></div>
+    </div>
+  );
+};
+
+export default Movies;
