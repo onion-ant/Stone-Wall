31e87fcce8626e64f2fdaa2092a9bde8448df9aa
Felipe Ferreira <fefelipe21@gmail.com>
2024-04-30 11:53:15 -0300

    add: questions section

--- StoneWall/src/Component/Questions/Questions.jsx
+++ StoneWall/src/Component/Questions/Questions.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import styles from './Questions.module.css'
+import styles from './Questions.module.css';
+import CardFaq from './CardFaq';
 
 const Questions = () => {
   return (
-    <div className={`${styles.main} container margin-top`}>
+    <section className={`${styles.main} container margin-top`}>
       <h1 className={styles.mainText}>Como funciona?</h1>
-    </div>
+      <CardFaq
+        tittle={'Analise os Preços'}
+        text={
+          'Descubra quanto cada serviço custa e se encaixa no seu bolso. Lembre-se de considerar o número de telas e a qualidade de transmissão.'
+        }
+        type={'cifrao'}
+      />
+      <CardFaq
+        tittle={'Decida com Confiança'}
+        text={
+          'Escolha o streaming que atenda às suas necessidades e comece a aproveitar o melhor do entretenimento.'
+        }
+        type={'lupa'}
+      />
+      <CardFaq
+        tittle={'Explore as Opções'}
+        text={
+          'Navegue pelos serviços de streaming e compare suas características. Veja quais oferecem os filmes e séries que você ama.'
+        }
+        type={'cifrao'}
+      />
+    </section>
   );
 };
 
