9e6292453d292d6b0dfba2b140f7c5a3ecbf51b1
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-02 14:05:23 -0300

    Create GetItemsByGenreAsync method

--- Backend/StoneWall/StoneWall/Services/StreamingServicesService.cs
+++ Backend/StoneWall/StoneWall/Services/StreamingServicesService.cs
@@ -76,6 +76,47 @@ namespace StoneWall.Services
             };
             return response;
         }
+        public async Task<ItemStreamingPaginationHelper> GetItemsByGenreAsync(string streamingId, int pageNumber)
+        {
+            int totalPages = await GetTotalPages(streamingId);
+            if ((totalPages < pageNumber || pageNumber == 0) && totalPages != 0)
+            {
+                throw new PageException("Invalid pageNumber");
+            }
+            var streamingItems = await _context.Item_Streaming
+                .AsNoTracking()
+                .Include(Is => Is.Item)
+                .ThenInclude(It => It.Genres)
+                .Where(Is => Is.StreamingId == streamingId)
+               .Select(Is => new ItemStreaming
+               {
+                   Item = new Item
+                   {
+                       TmdbId = Is.Item.TmdbId,
+                       Title = Is.Item.Title,
+                       OriginalTitle = Is.Item.OriginalTitle,
+                       Popularity = Is.Item.Popularity,
+                       Type = Is.Item.Type,
+                       Genres = Is.Item.Genres
+                   },
+                   Type = Is.Type,
+                   Link = Is.Link,
+               })
+                .OrderByDescending(Is => Is.Item.Popularity)
+                .Skip((pageNumber - 1) * _pageSize)
+                .Take(_pageSize)
+                .ToListAsync();
+            if (!streamingItems.Any())
+            {
+                throw new NotFoundException($"Theres no registered item from {streamingId}");
+            }
+            var response = new ItemStreamingPaginationHelper()
+            {
+                ItemsStreaming = streamingItems,
+                LastPage = totalPages,
+            };
+            return response;
+        }
         public async Task<ItemStreamingPaginationHelper> CompareStreamings(string streamingExclusive, string streamingExcluded, int pageNumber)
         {
             int totalPages = await GetTotalPages(streamingExclusive, streamingExcluded);
