8fb3e55c242838e247ba55471404daaaedb7e32c
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-01 21:12:00 -0300

    rename Carousel

--- StoneWall/src/Component/HomeStreamingCarousel/CarouselMenu.jsx
+++ StoneWall/src/Component/HomeStreamingCarousel/CarouselMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from './CarouselMenu.module.css';
-import Carousel from './ServicesCarousel';
+import Carousel from './Carousel';
 
 const CarouselMenu = () => {
   const [images, setImages] = React.useState([]);
--- StoneWall/src/Component/Movies/Movies.jsx
+++ StoneWall/src/Component/Movies/Movies.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+
 import styles from './Movies.module.css';
 
 const Movies = () => {
   return (
     <div className={styles.background}>
-      <div className={`${styles.movies} 'container'`}></div>
+      <div className={`${styles.movies} 'container'`}>
+      </div>
     </div>
   );
 };
