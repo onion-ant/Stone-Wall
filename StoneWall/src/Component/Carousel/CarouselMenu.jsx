e82283e023305ef642946a6bc8da22ec9414003b
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-01 21:03:35 -0300

    Carousel refactored

---
+++ StoneWall/src/Component/Carousel/CarouselMenu.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import styles from './CarouselMenu.module.css';
+import Carousel from './ServicesCarousel';
+
+const CarouselMenu = () => {
+  const [images, setImages] = React.useState([]);
+  useEffect(() => {
+    setImages([
+      "../../Assets/SquareHboMax.svg",
+      "../../Assets/SquarePrime.svg",
+      "../../Assets/SquareNetflix.svg",
+      "../../Assets/SquareAppleTV.svg",
+    ]);
+  }, []);
+
+  return (
+    <div className={styles.barra}>
+      <section className={`${styles.mainMenu} container margin-top`}>
+        <div className={`${styles.itemsCenter}`}>
+          <h1 className={styles.texto}>
+            DESCUBRA O <br />
+            MELHOR PARA VOCÊ
+          </h1>
+          <div className={styles.carousel}>
+            <Carousel direction={false} stop={false} images={images} />
+            <Carousel direction={true} stop={false} images={images} />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default CarouselMenu;
