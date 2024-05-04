9a373e88c46b872e0d159d711cb766b91b7171f6
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-04 01:48:38 -0300

    Create ErrorDetails entitie

---
+++ Backend/StoneWall/StoneWall/Entities/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace StoneWall.Entities
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public string? Trace { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
