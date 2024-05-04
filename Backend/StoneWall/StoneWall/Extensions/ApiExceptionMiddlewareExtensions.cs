ddcad9dd36f5c858668196e93825a9a310197ad2
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-04 02:23:17 -0300

    Create ApiExceptionMiddlewareExtensions to handdle global exceptions

---
+++ Backend/StoneWall/StoneWall/Extensions/ApiExceptionMiddlewareExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using StoneWall.Entities;
+using System.Net;
+
+namespace StoneWall.Extensions
+{
+    public static class ApiExceptionMiddlewareExtensions
+    {
+        public static void ConfigureException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Trace = contextFeature.Error.StackTrace
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
