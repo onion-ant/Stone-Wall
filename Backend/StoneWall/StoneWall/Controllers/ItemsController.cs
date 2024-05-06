2d26c539ce62efa9fb0e21114dddb06cb2e3e104
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-06 18:21:00 -0300

    Create ItemsController class

---
+++ Backend/StoneWall/StoneWall/Controllers/ItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoneWall.Entities;
+using StoneWall.Entities.Enums;
+using StoneWall.Helpers;
+using StoneWall.Services;
+using StoneWall.Services.Exceptions;
+
+namespace StoneWall.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsService _itemsService;
+        private readonly ITmdbService _tmdbService;
+        public ItemsController(IItemsService itemsService, ITmdbService tmdbService)
+        {
+            _itemsService = itemsService;
+            _tmdbService = tmdbService;
+        }
+    }
+}
