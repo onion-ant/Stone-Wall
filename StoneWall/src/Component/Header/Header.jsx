6e77832d0844b438ffefe96f8e1581cabfa49fc2
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-06 20:21:05 -0300

    add burguer menu

--- StoneWall/src/Component/Header/Header.jsx
+++ StoneWall/src/Component/Header/Header.jsx
@@ -1,14 +1,40 @@
+import React from 'react';
 import Button from '../Button/Button';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const ref = React.useRef(null);
+  const refButton = React.useRef(null);
+  React.useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        refButton.current.contains(event.target) &&
+        !ref.current.classList.contains(styles.active)
+      ) {
+        ref.current.classList.add(styles.active);
+        refButton.current.classList.add(styles.active);
+      } else if (
+        (!ref.current.contains(event.target) &&
+          !refButton.current.contains(event.target)) ||
+        (refButton.current.contains(event.target) &&
+          ref.current.classList.contains(styles.active))
+      ) {
+        ref.current.classList.remove(styles.active);
+        refButton.current.classList.remove(styles.active);
+      }
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+  }, []);
   return (
     <div className={`${styles.header} container`}>
       <Link to={'/'} className={styles.titulo}>
         STONEWALL
       </Link>
-      <div className={styles.rightSide}>
+      <div className={`${styles.rightSide}`} ref={ref}>
         <Link to={'/'} className={styles.link}>
           Analise
         </Link>
@@ -17,6 +43,9 @@ const Header = () => {
         </Link>
         <Button>ENTRE</Button>
       </div>
+      <button className={styles.button} ref={refButton}>
+        Menu
+      </button>
     </div>
   );
 };
