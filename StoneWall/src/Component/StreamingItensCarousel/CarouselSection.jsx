4c8e8bbfd232f0050355f6e04345d965edaa3764
Felipe Ferreira <fefelipe21@gmail.com>
2024-05-07 15:46:22 -0300

    add catch errors

--- StoneWall/src/Component/StreamingItensCarousel/CarouselSection.jsx
+++ StoneWall/src/Component/StreamingItensCarousel/CarouselSection.jsx
@@ -4,6 +4,7 @@ import styles from './CarouselSection.module.css';
 
 const CarouselSection = () => {
   const [autoPlay, setAutoPlay] = React.useState(true);
+  const [error, setError] = React.useState();
   const [current, setCurrent] = React.useState([1, 2, 0]);
   const [images, setImages] = React.useState([]);
   const intervalRef = React.useRef(null);
@@ -20,49 +21,53 @@ const CarouselSection = () => {
     intervalRef.current = null;
   };
   React.useEffect(() => {
-    fetch('https://picsum.photos/200/300').then((x) => {
-      setImages((previous) => [...previous, x.url]);
-    });
-    fetch('https://picsum.photos/200/300').then((x) => {
-      setImages((previous) => [...previous, x.url]);
-    });
-    fetch('https://picsum.photos/200/300').then((x) => {
-      setImages((previous) => [...previous, x.url]);
-    });
+    fetch('https://localhost:7282/Items?offset=3')
+      .then((x) => x.json())
+      .then((xs) => {
+        setImages(
+          xs.items.map((x) => {
+            return x.posterPath;
+          }),
+        );
+        setError(false);
+      })
+      .catch(() => {
+        setError(true); // <== this **WILL** be invoked on exception
+      });
     incrementCounter();
   }, []);
   return (
-    <div className={styles.background}>
-      <div
-        className={`${styles.movies} container'`}
-        onMouseEnter={() => {
-          resetCounter();
-          setAutoPlay(false);
-        }}
-        onMouseLeave={() => {
-          incrementCounter();
-          setAutoPlay(true);
-        }}
-      >
-        {images.map((image, index) => (
-          <img
-            src={image}
-            key={index}
-            alt=""
-            className={
-              autoPlay == false && current[index] == 1
-                ? `carousel_item_` + current[index] + ' carousel_paused'
-                : `carousel_item_` + current[index]
-            }
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      {!error && (
+        <div className={styles.background}>
+          <div
+            className={`${styles.movies} container'`}
+            onMouseEnter={() => {
+              resetCounter();
+              setAutoPlay(false);
+            }}
+            onMouseLeave={() => {
+              incrementCounter();
+              setAutoPlay(true);
+            }}
+          >
+            {images.map((image, index) => (
+              <img
+                src={image}
+                key={index}
+                alt=""
+                className={
+                  autoPlay == false && current[index] == 1
+                    ? `carousel_item_` + current[index] + ' carousel_paused'
+                    : `carousel_item_` + current[index]
+                }
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
-};
-
-CarouselSection.propTypes = {
-  images: PropTypes.array,
 };
 
 export default CarouselSection;
