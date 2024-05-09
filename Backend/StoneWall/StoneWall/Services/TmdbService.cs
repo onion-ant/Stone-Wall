a455d3eaab4359a7b90f7eafab3f3093a5d550d0
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-09 03:07:11 -0300

    Optmize TmdbService

--- Backend/StoneWall/StoneWall/Services/TmdbService.cs
+++ Backend/StoneWall/StoneWall/Services/TmdbService.cs
@@ -3,7 +3,9 @@ using StoneWall.Data;
 using StoneWall.Entities;
 using StoneWall.Entities.Enums;
 using StoneWall.Helpers;
+using StoneWall.Services.Exceptions;
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace StoneWall.Services
@@ -18,45 +20,56 @@ namespace StoneWall.Services
             TmdbKey = config["ApiKey"];
             _client = client;
         }
-        public async Task GetItemAsync(Item Item,string language="pt-BR")
+        public async Task GetItemAsync(Item Item,string language="pt-BR",string sizeParams = "original")
         {
-            HttpRequestMessage request;
-            if (Item.Type == ItemType.movie)
+            string url = Item.Type == ItemType.movie ? "https://api.themoviedb.org/3/movie/" : "https://api.themoviedb.org/3/tv/";
+            try
             {
-                request = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri($"https://api.themoviedb.org/3/movie/{Item.TmdbId}?language={language}&api_key={TmdbKey}")
-                };
-                HttpResponseMessage response = null;
-                try
-                {
-                    response = await _client.SendAsync(request);
-                }
-                catch(Exception ex)
-                {
-                    await Console.Out.WriteLineAsync(ex.Message);
-                }
-                string? body = await response.Content.ReadAsStringAsync();
-                TmdbJsonHelper? itemJsonHelper = JsonConvert.DeserializeObject<TmdbJsonHelper>(body);
-                Item.Overview = itemJsonHelper.overview;
-                Item.PosterPath = "https://image.tmdb.org/t/p/original/" + itemJsonHelper.poster_path;
-                Item.ReleaseYear = int.Parse(itemJsonHelper.release_date.Split('-')[0]);
-            }
-            else
-            {
-                request = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri($"https://api.themoviedb.org/3/tv/{Item.TmdbId}?language=pt-BR&api_key={TmdbKey}")
-                };
+                var request = RequestBuilder(Item.TmdbId, language, url);
                 var response = await _client.SendAsync(request);
                 string? body = await response.Content.ReadAsStringAsync();
                 TmdbJsonHelper? itemJsonHelper = JsonConvert.DeserializeObject<TmdbJsonHelper>(body);
-                Item.Overview = itemJsonHelper.overview;
-                Item.PosterPath = "https://image.tmdb.org/t/p/original/" + itemJsonHelper.poster_path;
-                Item.ReleaseYear = int.Parse(itemJsonHelper.first_air_date.Split('-')[0]);
-                Item.LastAirYear = int.Parse(itemJsonHelper.last_air_date.Split('-')[0]);
+                Item.Overview = itemJsonHelper!.overview;
+                Item.PosterPath = $"https://image.tmdb.org/t/p/{sizeParams}/" + itemJsonHelper.poster_path;
+                if (itemJsonHelper.release_date != null)
+                {
+                    Item.ReleaseYear = ParseYear(itemJsonHelper.release_date);
+                }
+                else
+                {
+                    Item.ReleaseYear = ParseYear(itemJsonHelper.first_air_date);
+                    Item.LastAirYear = ParseYear(itemJsonHelper.last_air_date);
+                    Item.SeasonsCount = itemJsonHelper.number_of_seasons;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ExternalApiException(ex.Message);
+            }
+        }
+        private HttpRequestMessage RequestBuilder(int? TmdbId, string language, string url)
+        {
+            return new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{url}{TmdbId}?language={language}"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", $"Bearer {TmdbKey}" },
+                },
+            };
+        }
+        private int ParseYear(string date)
+        {
+            var yearString = date.Split('-')[0];
+            if (int.TryParse(yearString, out int year))
+            {
+                return year;
+            }
+            else
+            {
+                return 0;
             }
         }
     }
