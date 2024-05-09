7963a4f9ffb92446a027952e29377c67703e99c7
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-09 06:37:37 -0300

    Create PagedList class to change pagination implementation

---
+++ Backend/StoneWall/StoneWall/Pagination/PagedList.cs
@@ -0,0 +1,28 @@
+﻿namespace StoneWall.Pagination
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        public PagedList(T[] items,int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count/ (double)PageSize);
+
+            AddRange(items);
+        }
+        public static PagedList<T> ToPagedList(IQueryable<T> source,int pageNumber,int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToArray();
+
+            return new PagedList<T>(items,count,pageNumber,pageSize);
+        }
+    }
+}
