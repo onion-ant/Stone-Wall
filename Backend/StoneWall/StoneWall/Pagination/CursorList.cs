cdafc5edf66202773ba955134c032ac3a94c9d8f
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-16 01:49:27 -0300

    Create CursorList class

---
+++ Backend/StoneWall/StoneWall/Pagination/CursorList.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+
+namespace StoneWall.Pagination
+{
+    public sealed class CursorList<T> : List<T>
+    {
+        public int? Cursor {  get; set; }
+        public int Limit { get; set; }
+
+        public bool HasNext => Cursor != null;
+
+        public CursorList(List<T> items, int limit , int? cursor)
+        {
+            Cursor = cursor;
+            Limit = limit;
+
+            AddRange(items);
+        }
+
+        public static async Task<CursorList<T>> ToCursorListAsync(IQueryable<T> source, int limit , int? cursor)
+        {
+            var items = await source.Take(limit).ToListAsync();
+
+            return new CursorList<T>(items, limit, cursor);
+        }
+    }
+}
