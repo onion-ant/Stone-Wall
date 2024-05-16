84b1480dacf4ffc164958724ad1541a04d401532
Antonio Vidal <antoniohenriquebv@gmail.com>
2024-05-16 17:28:23 -0300

    Implement cursor in items

--- Backend/StoneWall/StoneWall/Pagination/CursorList.cs
+++ Backend/StoneWall/StoneWall/Pagination/CursorList.cs
@@ -5,24 +5,23 @@ namespace StoneWall.Pagination
 {
     public sealed class CursorList<T> : List<T>
     {
-        public int? Cursor {  get; set; }
+        public string? NextCursor {  get; set; }
         public int Limit { get; set; }
 
-        public bool HasNext => Cursor != null;
+        public bool HasNext => NextCursor != null;
 
-        public CursorList(List<T> items, int limit , int? cursor)
+        public CursorList(List<T> items, int limit)
         {
-            Cursor = cursor;
             Limit = limit;
 
             AddRange(items);
         }
 
-        public static async Task<CursorList<T>> ToCursorListAsync(IQueryable<T> source, int limit , int? cursor)
+        public static async Task<CursorList<T>> ToCursorListAsync(IQueryable<T> source, int limit)
         {
             var items = await source.Take(limit).ToListAsync();
 
-            return new CursorList<T>(items, limit, cursor);
+            return new CursorList<T>(items, limit);
         }
     }
 }
